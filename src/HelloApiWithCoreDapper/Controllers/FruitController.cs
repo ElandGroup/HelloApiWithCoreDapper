8c71b37bc065c633bdb1294cb42443e47f18f57f
elandcloud <xiaoxm_001@outlook.com>
2016-07-23 16:14:23 +0800

    first

---
+++ src/HelloApiWithCoreDapper/Controllers/FruitController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloApiWithCoreDapper.Service;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloApiWithCoreDapper.Controllers
+{
+    [Route("api/[controller]")]
+    public class FruitController : Controller
+    {
+        IFruitService _fruitService = new FruitService();
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var fruitDtoList = await _fruitService.FruitQuery();
+            return new OkObjectResult(fruitDtoList);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/HelloApiWithCoreDapper/Service/FruitService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using HelloApiWithCoreDapper.Models;
+
+namespace HelloApiWithCoreDapper.Service
+{
+    public interface IFruitService
+    {
+        Task<IEnumerable<FruitDto>> FruitQuery();
+    }
+    public class FruitService : IFruitService
+    {
+        string connStr = @"Data Source=qds114812583.my3w.com,1433;Initial Catalog=qds114812583_db;
+            User ID=qds114812583;password=Eland123;Integrated Security=False";
+        public async Task<IEnumerable<FruitDto>> FruitQuery()
+        {
+            string sqlText = @"SELECT * FROM  Fruit";
+            using (var conn = new SqlConnection(connStr))
+            {
+                return await conn.QueryAsync<FruitDto>(sqlText);
+            }
+        }
+
+    }
+}
